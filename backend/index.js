407543c4d939b4fb76360f806d4a2ef260c5a0ce
darkg123 <sgogoi628@gmail.com>
2021-09-26 11:31:10 +0530

    Backend updated with userschema and pinschema,for frontend added a mapbox and marker with a room icon

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const app = express();
+const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins")
+
+
+dotenv.config();
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected')
+  })
+  .catch((err) => console.log(err))
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(5000, () => {
+  console.log("Backened server is running")
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { Room } from "@material-ui/icons";
+
+function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4
+  });
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+      >
+        <Marker
+          latitude={48.858093}
+          longitude={2.294694}
+          offsetLeft={-20}
+          offsetTop={-10}
+        ><Room />
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;
